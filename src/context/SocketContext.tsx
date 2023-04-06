88db382cc3c01d62f05b09feb680154e8d0f4e25
Thiago Oliveira <thiagooliveira.k.on@gmail.com>
2023-04-06 17:11:56 -0300

    this is a test

--- src/context/SocketContext.tsx
+++ src/context/SocketContext.tsx
@@ -59,6 +59,8 @@ function SocketProvider({ children }: SocketContextProviderProps) {
         setStream(currentStream)
 
         myVideo.current.srcObject = currentStream
+
+        console.log(myVideo.current.srcObject)
       })
 
     socket.on("me", (id) => setMe(id))
