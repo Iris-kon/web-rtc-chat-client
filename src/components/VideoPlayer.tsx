0158be71c642e79300fa3e13d1e5eedb02451705
Thiago Oliveira <thiagooliveira.k.on@gmail.com>
2023-04-10 20:31:18 -0300

    remove video constraints

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -35,11 +35,6 @@ export function VideoPlayer() {
     useContext(SocketContext)
   const classes = useStyles()
 
-  const videoConstraints = {
-    width: 1280,
-    height: 720,
-    facingMode: "user",
-  }
   return (
     <Grid container className={classes.gridContainer}>
       {/**Our own Video */}
